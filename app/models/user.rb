4e7a0c7b8ac729a767374eac32cfcced8daac5eb
tokorotenn <hk.1003.abc@gmail.com>
2021-08-06 19:29:32 +0900

    userモデルにバリデーションを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # 入力必須のバリデーション
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
+
+    # 全角（漢字・ひらがな・カタカナ）のバリデーション
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message:"First name is invalid. Input full-width characters"} 
+    validates :family_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message:"Last name is invalid. Input full-width characters"} 
+
+    # 全角（カタカナ）のバリデーション
+    validates :first_name_reading, format: {with: /\A[ァ-ヶー]+\z/, message:"First name kana is invalid. Input full-width katakana characters"}
+    validates :family_name_reading, format: {with: /\A[ァ-ヶー]+\z/, message:"Last name kana is invalid. Input full-width katakana characters"}
+  end
+
+  # 英数字混合必須のバリデーション（英字かつ数字）
+  validates :encrypted_password, format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i , message:"Password is invalid. Include both letters and numbers"}
 end 
