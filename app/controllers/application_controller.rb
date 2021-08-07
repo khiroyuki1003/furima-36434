161010e57c57d5c4a11ee2012a5deb6277b93d27
tokorotenn <hk.1003.abc@gmail.com>
2021-08-07 15:42:17 +0900

    ユーザー新規登録機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :user_data_parameter, if: :devise_controller?
 
 
   private
@@ -9,4 +10,10 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+
+  def user_data_parameter
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :family_name, :first_name_reading, :family_name_reading, :birth_date])
+  end
 end
+
+
