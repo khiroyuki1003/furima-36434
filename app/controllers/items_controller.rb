549252125f882c0cc10c367ce9e5ad2dee7ed082
tokorotenn <hk.1003.abc@gmail.com>
2021-08-09 17:02:04 +0900

    ログアウト時のリダイレクト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :redirect_to_index, only: :new 
+
   def index
   end
 
@@ -8,5 +10,12 @@ class ItemsController < ApplicationController
 
   def create
 
+  end
+
+  private
+  def redirect_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 end
