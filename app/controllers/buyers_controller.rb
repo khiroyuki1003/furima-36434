7c04e67900a6b2aeb5d48dc3b2a3609db5202735
tokorotenn <hk.1003.abc@gmail.com>
2021-08-15 18:58:06 +0900

    buyerコントローラーの外部キーの取り込み設定を変更

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -27,7 +27,7 @@ class BuyersController < ApplicationController
   def buyer_params
     params.require(:buyer_receiver_address).permit(
       :postal_code, :prefectures_id, :city, :house_number, :building_name, :phone_number
-    ).merge(user_id: current_user.id, item_id: @item.id, token: params[:token]) 
+    ).merge(user: current_user, item: @item, token: params[:token]) 
   end
 
   ##Payjpを利用した決済処理
--- app/models/buyer_receiver_address.rb
+++ app/models/buyer_receiver_address.rb
@@ -1,12 +1,10 @@
 class BuyerReceiverAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefectures_id, :city, :house_number, :building_name, :phone_number, :buyer_id, :token
+  attr_accessor :user, :item, :postal_code, :prefectures_id, :city, :house_number, :building_name, :phone_number, :buyer_id, :token
 
   with_options presence: true do
     validates :token
-    validates :user_id
-    validates :item_id
-    validates :prefectures_id
+    validates :prefectures_id, numericality: {other_than: 1, message: "can't be blank"} 
     validates :city
     validates :house_number
     validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/ ,message:"is invalid. Enter it as follows (e.g. 123-4567)"}
@@ -16,7 +14,7 @@ class BuyerReceiverAddress
 
 
   def save
-    buyer = Buyer.create(user_id: user_id,item_id: item_id)
+    buyer = Buyer.create(user: user,item: item)
     ReceiverAddress.create(
       postal_code: postal_code, prefectures_id: prefectures_id, city: city, house_number: house_number, 
       building_name: building_name, phone_number: phone_number, buyer_id: buyer.id
