84a17f314481be32ef29143ca96351b1d617b79b
tokorotenn <hk.1003.abc@gmail.com>
2021-08-16 18:02:58 +0900

    receiver_addressモデルの外部キーのバリデーションを削除

--- app/models/receiver_address.rb
+++ app/models/receiver_address.rb
@@ -3,6 +3,5 @@ class ReceiverAddress < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefectures
 
-  validates :buyer_id, presence: true
 end
- 
+  
