228dacffa4ef56fc0e94ccf5c2d6735e3ba85493
tokorotenn <hk.1003.abc@gmail.com>
2021-08-16 16:02:28 +0900

    修正③：テストコード大枠修正

--- spec/models/buyer_receiver_address_spec.rb
+++ spec/models/buyer_receiver_address_spec.rb
@@ -7,49 +7,49 @@ RSpec.describe BuyerReceiverAddress, type: :model do
     @buyer_receiver_address = FactoryBot.build(:buyer_receiver_address, user_id: user.id, item_id: item.id)
   end
 
-  describe '商品購入できる時' do 
-    it '全ての項目が正しく入力されていれば購入できる' do
-      expect(@buyer_receiver_address).to be_valid
-    end
-    it 'building_nameが空でも購入できる' do
-      @buyer_receiver_address.building_name = ""
-      expect(@buyer_receiver_address).to be_valid
-    end
-  end
-
-  describe '商品購入できない時' do
-    it 'user_idが空では購入できない' do
-      @buyer_receiver_address.user_id = nil
-      @buyer_receiver_address.valid?
-      expect(@buyer_receiver_address.errors.full_messages ).to include("User can't be blank")
-    end
-    it 'item_idが空では購入できない' do
-      @buyer_receiver_address.item_id = nil
-      @buyer_receiver_address.valid?
-      expect(@buyer_receiver_address.errors.full_messages ).to include("Item can't be blank")
-    end
-    it 'tokenが無いと購入できない' do
-      @buyer_receiver_address.token = ""
-      @buyer_receiver_address.valid?
-      expect(@buyer_receiver_address.errors.full_messages ).to include("Token can't be blank")
-    end
-    it 'prefectures_idが選択されていないと購入できない' do
-      @buyer_receiver_address.prefectures_id = "1"
-      @buyer_receiver_address.valid?
-      expect(@buyer_receiver_address.errors.full_messages ).to include("Prefectures can't be blank")
-    end
-    it 'cityが空だと購入できない' do
-      @buyer_receiver_address.city = ""
-      @buyer_receiver_address.valid?
-      expect(@buyer_receiver_address.errors.full_messages ).to include("City can't be blank")
-    end
-    it 'house_numberが空だと購入できない' do
-      @buyer_receiver_address.house_number = ""
-      @buyer_receiver_address.valid?
-      expect(@buyer_receiver_address.errors.full_messages ).to include("House number can't be blank")
+  describe '商品購入機能' do 
+    context '商品購入できる時' do
+      it '全ての項目が正しく入力されていれば購入できる' do
+        expect(@buyer_receiver_address).to be_valid
+      end
+      it 'building_nameが空でも購入できる' do
+        @buyer_receiver_address.building_name = ""
+        expect(@buyer_receiver_address).to be_valid
+      end
     end
 
-    context 'postal_codeが正しくないと購入できない' do
+    context '商品購入できない時' do
+      it 'user_idが空では購入できない' do
+        @buyer_receiver_address.user_id = nil
+        @buyer_receiver_address.valid?
+        expect(@buyer_receiver_address.errors.full_messages ).to include("User can't be blank")
+      end
+      it 'item_idが空では購入できない' do
+        @buyer_receiver_address.item_id = nil
+        @buyer_receiver_address.valid?
+        expect(@buyer_receiver_address.errors.full_messages ).to include("Item can't be blank")
+      end
+      it 'tokenが無いと購入できない' do
+        @buyer_receiver_address.token = ""
+        @buyer_receiver_address.valid?
+        expect(@buyer_receiver_address.errors.full_messages ).to include("Token can't be blank")
+      end
+      it 'prefectures_idが選択されていないと購入できない' do
+        @buyer_receiver_address.prefectures_id = "1"
+        @buyer_receiver_address.valid?
+        expect(@buyer_receiver_address.errors.full_messages ).to include("Prefectures can't be blank")
+      end
+      it 'cityが空だと購入できない' do
+        @buyer_receiver_address.city = ""
+        @buyer_receiver_address.valid?
+        expect(@buyer_receiver_address.errors.full_messages ).to include("City can't be blank")
+      end
+      it 'house_numberが空だと購入できない' do
+        @buyer_receiver_address.house_number = ""
+        @buyer_receiver_address.valid?
+        expect(@buyer_receiver_address.errors.full_messages ).to include("House number can't be blank")
+      end
+    
       it 'postal_codeが空だと購入できない' do
         @buyer_receiver_address.postal_code = ""
         @buyer_receiver_address.valid?
@@ -65,9 +65,7 @@ RSpec.describe BuyerReceiverAddress, type: :model do
         @buyer_receiver_address.valid?
         expect(@buyer_receiver_address.errors.full_messages ).to include('Postal code is invalid. Enter it as follows (e.g. 123-4567)')
       end
-    end
-
-    context 'phone_numberが正しくないと購入できない' do 
+    
       it 'phone_numberが空だと登録できない' do
         @buyer_receiver_address.phone_number = ""
         @buyer_receiver_address.valid?
